33fca01490352806f1534d93b6b877b4e9bfe1fa
EC2 Default User <ec2-user@ip-172-31-11-159.us-west-1.compute.internal>
2021-08-13 04:17:58 +0000

    asdf

--- main.py
+++ main.py
@@ -8,14 +8,26 @@ import json
 UDP_IP = "127.0.0.1"
 UDP_PORT = 31000
 
+iamMap = {}
+iamDef = {}
+
 def handle_api_call(msg):
-    #print(msg)
-    
     service = msg['Service']
     api = msg['Api']
-    print('ApiMsg[%s] %s' % (service,api))
+    print('ApiMsg %s:%s' % (service,api))
+
+def loadMaps():
+   with open('map.json') as f:
+       iamMap = json.loads(f.read())
+   with open('iam_definition.json') as f:
+       iamDef = json.loads(f.read())
+
 
 def main():
+    loadMaps()
+
+    log_file = open('csm.log', 'w')
+
     sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
     sock.bind((UDP_IP, UDP_PORT))
@@ -24,11 +36,14 @@ def main():
         data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
         data_str = data.decode('utf-8')
         data_json = json.loads(data_str)
-        print("received message: %s" % data_json)
+        log_file.write(data_str)
+        #print("received message: %s" % data_json)
 
         if 'Type' in data_json:
             if data_json['Type'] == 'ApiCall':
                 handle_api_call(data_json)
+            else:
+                print(data_str)
 
 if __name__ == '__main__':
     main()
